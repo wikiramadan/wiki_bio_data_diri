797d9029c3a9c362fa8c65b6e7824787c41552b0
wikiramadan <wikiramadan5@gmail.com>
2023-06-16 15:41:54 +0700

    bio data diri wiki

---
+++ data_diri_wiki/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+class programinput
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("======DATA PESERTA DIDIK SMK NEGERI 1 LUBUK SIKAPING");
+        Console.Write(" Tuliskan nama kamu :");
+        string nama = Console.ReadLine();
+        Console.Write(" Tuliskan alamat kamu :");
+        string alamat = Console.ReadLine();
+        Console.Write("Tuliskan kelas kamu :");
+        string kelas = Console.ReadLine();
+        Console.Write("Tuliskan jurusan kamu :");
+        string jurusan = Console.ReadLine();
+        Console.Write("Tuliskan agama kamu :");
+        string agama = Console.ReadLine();
+        Console.Write("Tuliskan hobby kamu :");
+        string hobby = Console.ReadLine();
+        Console.Write("Tuliskan cita cita kamu :");
+        string cita_cita = Console.ReadLine();
+        Console.Write("Tuliskan kamu anak ke :")
+; string anakke = Console.ReadLine();
+        Console.WriteLine("=======DATA ORANG TUA PESERTA DIDIK SMK 1 LUBUK SIKAPING");
+        Console.Write("Tuliskan nama ayah kamu :");
+        string nama_ayah = Console.ReadLine();
+        Console.Write("Tuliskan nama ibu kamu :");
+        string nama_ibu = Console.ReadLine();
+        Console.Write("Tuliskan pekerjaan :");
+        string pekerjaan = Console.ReadLine();
+        Console.Write("Tuliskan no hp :");
+        string nohp = Console.ReadLine();
+        Console.Write("Tuliskan alamat :");
+        string ala_mat = Console.ReadLine();
+        Console.Write("Tuliskan status anak :");
+        string status_anak = Console.ReadLine();
+
+        Console.WriteLine("========================================");
+        Console.WriteLine("nama lengkap : {0}", nama);
+        Console.WriteLine("alamat : {0}", alamat);
+        Console.WriteLine("kelas : {0}", kelas);
+        Console.WriteLine("jurusan {0}", jurusan);
+        Console.WriteLine("agama : {0}", agama);
+        Console.WriteLine("hobby : {0}", hobby);
+        Console.WriteLine("cita cita : {0}", cita_cita);
+        Console.WriteLine("anak ke : {0}", anakke);
+        Console.WriteLine("========DATA ORANG TUA PESERTA DIDIK SMK NEGERI 1 LUBUK SIKAPING");
+        Console.WriteLine("nama ayah : {0}", nama_ayah);
+        Console.WriteLine("nama ibu : {0}", nama_ibu);
+        Console.WriteLine("pekerjaan : {0}", pekerjaan);
+        Console.WriteLine("no hp : {0}", nohp);
+        Console.WriteLine("ala_mat : {0}", ala_mat);
+        Console.WriteLine("status anak : {0}", status_anak);
+
+
+    }
+}
